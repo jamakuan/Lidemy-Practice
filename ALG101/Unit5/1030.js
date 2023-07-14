1b394f12f141c57c2e7ea209e05f4f1824aeffc6
Jama <kuan2221@gmail.com>
2023-07-14 11:30:41 +0800

    ALG101 Unit 5 complete 1030.js

---
+++ ALG101/Unit5/1030.js
@@ -0,0 +1,15 @@
+// abbba = abbba
+
+function ad(str){
+	let result = ''
+	for(i = str.length-1; i >= 0; i--) {
+		result += str[i]
+		
+	} if(result == str) {
+		return true
+	}else return false
+}
+console.log(ad('aba'))
+
+
+// method 2 
