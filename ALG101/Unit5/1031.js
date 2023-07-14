31b691a59958e76ef0305c18303eb23f7b277f0b
Jama <kuan2221@gmail.com>
2023-07-14 11:41:09 +0800

    ALG101 Unit 5 complete 1031.js

---
+++ ALG101/Unit5/1031.js
@@ -0,0 +1,17 @@
+/*
+
+當一個數字是某個正整數的平方時，我們稱這個數為完全平方數
+
+例如說 1、9、16、25、36、49，這些都是完全平方數
+
+今天給你一個數字 N，請你求出 1~N 裡面完全平方數的總和
+
+*/
+function perfect_square(n) {
+	let result = 0
+	let s = Math.floor(Math.sqrt(n))
+	for(i = 1; i<=s; i++) {
+		result += i*i
+	}return result
+}
+console.log(perfect_square(40))
