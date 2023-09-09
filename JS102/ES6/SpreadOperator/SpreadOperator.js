54393c823c276d2e94c50f8617411e56c3515476
Jama <kuan2221@gmail.com>
2023-09-09 17:52:51 +0800

    JS102 Spread Operator

---
+++ JS102/ES6/SpreadOperator/SpreadOperator.js
@@ -0,0 +1,35 @@
+var arr = [1, 2, 3]
+var arr2 = [4, 5, 6, ...arr]
+
+console.log(arr2)
+
+function add(a, b, c) {
+	return a + b + c
+}
+
+var arr=[1, 2, 3]
+console.log(add(...arr))
+
+var obj1 = {
+	a: 1, 
+	b: 2
+}
+
+var obj2 = {
+	z: 1
+}
+
+var obj3 = {
+	...obj1, 
+	c: 3
+}
+
+console.log(obj3)
+
+//
+var nestedArray = [4]
+var arr = [1, 2, 3, nestedArray]
+console.log('arr:', arr)
+var arr2 = [... arr]
+console.log('arr2:', arr2)
+console.log(arr[3] === arr2[3])
