cbe71a3fc606285338abe5686246ede38dcc13ec
U22.04-jama <kuan2221@gmail.com>
2023-09-19 15:55:52 +0800

    Create a super easy server

--- Net101/HTTP/request.js
+++
@@ -1,9 +0,0 @@
-const request = require("request");
-request(
-  "https://www.npmjs.com/package/request",
-  function (error, response, body) {
-    // console.error("error:", error); // Print the error if one occurred
-    // console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-    console.log("body:", body); // Print the HTML for the Google homepage.
-  },
-);
